7d86821fe61e09758faa9c5785ad4286759df471
nicmosc <nmoscholios@gmail.com>
2018-01-01 11:10:30 +0100

    set lower pixel ratio for now

--- app/components/engine/core/Renderer.js
+++ app/components/engine/core/Renderer.js
@@ -1,7 +1,8 @@
 export class Renderer extends THREE.WebGLRenderer {
   init(width, height) {
+    // console.log(window.devicePixelRatio);
     this.setClearColor( 0xffffff );
-    this.setPixelRatio( window.devicePixelRatio );
+    this.setPixelRatio( 1 );
     this.setSize( width, height );
 
     this.gammaInput = true;
