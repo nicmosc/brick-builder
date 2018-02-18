27f76df6e4ff29cfc8317b50ca3ac728e566e9eb
nicmosc <nmoscholios@gmail.com>
2018-02-18 19:09:11 +0100

    add some new bricks and colours

--- app/utils/constants.js
+++ app/utils/constants.js
@@ -1,18 +1,15 @@
-// export const dimensions = { x: 3, z: 1 };
 export const base = 25;
+
 
 export const bricks = [
   { x: 1, z: 1 },
   { x: 2, z: 1 },
   { x: 2, z: 2 },
-  { x: 2, z: 3 },
-  { x: 2, z: 4 },
+  { x: 3, z: 1 },
+  { x: 3, z: 2 },
+  { x: 4, z: 1 },
+  { x: 4, z: 2 },
 ];
 
-// export const height = (base * 2) / 1.5;
-// export const width = base * dimensions.x;
-// export const depth = base * dimensions.z;
 
-// export const colors = ['#b9140a', '#1e6914', '#1a3fcc', '#d49417'];
-
-export const colors = ['#FF0000', '#00DE00', '#0011CF', '#F0E100', '#FF9800', '#FFFFFF', '#000000'];
+export const colors = ['#FF0000', '#FF9800', '#F0E100', '#00DE00', '#A1BC24', '#0011CF', '#FFFFFF', '#000000', '#652A0C' ];
