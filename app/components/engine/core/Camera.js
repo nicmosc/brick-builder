38d1ce31862828fe94c53a99522fac9ff167fba9
nicmosc <nmoscholios@gmail.com>
2018-02-18 19:10:58 +0100

    change starting camera position

--- app/components/engine/core/Camera.js
+++ app/components/engine/core/Camera.js
@@ -1,6 +1,6 @@
 export class PerspectiveCamera extends THREE.PerspectiveCamera {
   init() {
-    this.position.set(600,500,500);
+    this.position.set(-600,500,500);
     this.lookAt( new THREE.Vector3() );
   }
 }
